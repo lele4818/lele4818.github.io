d6390786b3cc4fae43a8624ae879499db62efeb6
qinguangming <13006397736@163.com>
2019-10-10 14:15:49 +0800

    font-size

--- vue-cli-project/src/components/Article.vue
+++ vue-cli-project/src/components/Article.vue
@@ -62,11 +62,11 @@ body{
 	overflow-y: auto;
 }
 .content{
-	font-family: Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace;
 	line-height: 25px;
 	padding-top: 15px;
 	border-top:1px solid #EAEAEA ;
 	margin-top: 15px;
+	font-size: 14px;
 }
 .content code{
 	letter-spacing: initial;
